63959ead7d9dbd50db656f0a7548b41ead59f86b
Alexandr Retron <sashatk835@gmail.com>
2024-08-11 20:03:31 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TestHeader from "./components/testing/test-header"
+import TestFooter from "./components/testing/test-footer"
+
+function App(){
+  return <div>
+    <TestHeader/>
+    <TestFooter/>
+  </div>
+}
+
+export default App;
---
+++ src/components/testing/test-footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function TestFooter(){
+    return (
+    <footer className="test-header test-footer">
+        <h4>Alexandr Tkachyov</h4>
+    </footer>
+    )
+}
+
+export default TestFooter;
