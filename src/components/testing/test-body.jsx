0753850a6a6392131181cde0a40bc7b419a72611
Alexandr Retron <sashatk835@gmail.com>
2024-08-25 23:32:06 +0500

    Complete storing different question types

--- src/components/testing/test-body.jsx
+++ src/components/testing/test-body.jsx
@@ -12,14 +12,49 @@ function TestBody(props){
 
     // Temporary storing the question
     const question = {
-        questionStatement: "Which choice completes text the text with the most logical and precise word or phrase?",
+        testPart: 1,
+        section: 1,
         questionNumber: 1,
+        questionType: 1,
+        passage: [
+                    {
+                        type: "blurb", 
+                        content:[
+                                {
+                                    type:"standard",
+                                    content: "While researching a topic, a student has taken the following notes:"
+                                },
+                                ]
+                    },
+
+                    {
+                        type: "notes", 
+                        content:[
+                                    [   
+                                        {
+                                            type: "standard",
+                                            content:"Scientists have developed a “freeze-thaw” battery that can retain 92% of its charge after twelve weeks.",
+                                        }
+                                    ],
+
+                                    [   
+                                        {
+                                            type: "standard",
+                                            content:"Scientists have developed a “freeze-thaw” battery that can retain 92% of its charge after twelve weeks.",
+                                        }
+                                    ],
+                                ]
+                    },
+
+                    
+                ],
+
+        isMarked: false,
+        isAnswered: false,
+        questionStatement: "Which choice completes text the text with the most logical and precise word or phrase?",
         answerChoices: {
             values: ["A","B","C","D"],
             contents: ["scholarly", "melodic", "jarring", "personal"],
-        },
-        passage: {
-            passageText: <>In recommending Bao Phi's collection <i>Sông I Sing</i>, a librarian noted that pieces by the spoken-word poet don't lose their ______ nature when printed: the language has the same pleasant musical quality on the page as it does when performed by Phi.</>,
         },
     }
 
--- src/components/testing/test-passage-text.jsx
+++ src/components/testing/test-passage-text.jsx
@@ -1,18 +1,74 @@
 import React from "react";
 import styles from "../../styles/testPassageText.module.scss"
 
-function TestPassageText(props){
-    const {passageText} = props.passage;
+function TestPassageText({passage}){
+    
+    function fetchPassage (typeWithContent,index) {
+        if(typeWithContent.type==="blurb"){
+            return(
+                <div key={index} className={styles.blurb}>
+                    {typeWithContent.content.map((element,index) => fetchPassage (element,index))}
+                </div>
+            )
+        }
+        else if(typeWithContent.type==="header"){
+            return(
+                <div key={index} className={`${styles.passageHeader} testFont`}>{typeWithContent.content}</div>
+            )
+        }
+        else if(typeWithContent.type==="standard"){
+            return(
+                <span key={index} className={`testFont`}>{typeWithContent.content}</span>
+            )
+        }
+        else if(typeWithContent.type==="copyrights"){
+            return(
+                <div key={index} className={`${styles.passageCopyrights} testFont`}>©{typeWithContent.content}</div>
+            )
+        }
+        else if(typeWithContent.type==="italics"){
+            return(
+                <span key={index} className={`${styles.italicText} testFont`}>{typeWithContent.content}</span>
+            )
+        }
+        else if(typeWithContent.type==="graph"){
+            return(
+                <div key={index} className={styles.graphContainer}>
+                    <img draggable={false} src = {typeWithContent.content} alt="graph"/>
+                </div>
+            )
+        }
+        else if(typeWithContent.type==="notes"){
+            return(
+                <div key={index} className={styles.notesContainer}>
+                    <ul>
+                        {typeWithContent.content.map((note,index) => {
+                           return (
+                                    <li className="testFont" key={index}> 
+                                        {note.map((typeWithContent,index) => fetchPassage(typeWithContent, index))} 
+                                    </li>
+                                )
+                        })}
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+
     return (
-        <div className={styles.container}>
-            <div className={styles.passage}>
-            {/* <p id={styles.blurb} className="testFont">The researching a topic, a student has taken the following notes:</p> */}
-            <p className="testFont">
-                {passageText}
-            </p>
+        <div className={`${styles.container}`}>
+
+            <div className={styles.passageContainer}>
+
+            {passage.map((typeWithContent, index)=> fetchPassage(typeWithContent, index))}
+
+
             </div>
         </div>
     )
 }
 
 export default TestPassageText;
+
+//passage: [[text, sda], [underlined, sda], [text, sda]]
